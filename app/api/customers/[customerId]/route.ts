69539f5ae94ef5145f66e5b7b87ec03f6cb8fbdf
LƯU MỸ NHẬT HUY <huylmnse181744@fpt.edu.vn>
2025-11-23 15:33:57 +0700

    fix customerId

--- app/api/customers/[customerId]/route.ts
+++ app/api/customers/[customerId]/route.ts
@@ -1,14 +1,15 @@
 import Customer from "@/lib/models/customer";
 import connectToDB from "@/lib/mongoDB";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
+  req: NextRequest,
   { params }: { params: Promise<{ customerId: string }> }
 ) => {
   try {
     await connectToDB();
     const { customerId } = await params;
-    const customer = await Customer.findOne({clerkId:customerId});
+    const customer = await Customer.findOne({ clerkId: customerId });
     if (!customer) {
       return new NextResponse("Can not find customerID", { status: 400 });
     }
